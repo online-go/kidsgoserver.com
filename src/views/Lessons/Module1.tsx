56f89e93b0ede05119631cd3a02d2d060026b01d
Akita Noek <anoek@online-go.com>
2022-08-15 09:07:01 -0600

    lint

--- src/views/Lessons/Module1.tsx
+++ src/views/Lessons/Module1.tsx
@@ -30,7 +30,10 @@ class Page1 extends Module1 {
     text(): JSX.Element | Array<JSX.Element> {
         return [
             <span>In Go we place stones on the lines, not in the squares!</span>,
-            <span>On Earth the stones are Black and White, but in Interstellar Go we play with Pulsar Pink and Whammo White. </span>,
+            <span>
+                On Earth the stones are Black and White, but in Interstellar Go we play with Pulsar
+                Pink and Whammo White.{" "}
+            </span>,
             <span>For the Earthlings we will call them Black and White in these lessons. </span>,
             <span>Black always goes first.</span>,
         ];
